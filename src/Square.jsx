b3cd4cf2a9e8dcc820d91bb3d20a6a8819081a91
Ken Okerlund <ken.okerlund@gmail.com>
2023-05-18 19:44:17 -0600

    Project Complete

---
+++ src/Square.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Square = (props) => {
+  const handleClick = () => {
+    if (!props.squareValue) {
+      if (props.player) {
+        props.squares.splice(props.index, 1, "X");
+        props.setSquares(props.squares);
+        props.setPlayer(!props.player);
+      } else {
+        props.squares.splice(props.index, 1, "O");
+        props.setSquares(props.squares);
+        props.setPlayer(!props.player);
+      }
+    }
+    console.log(props.squares)
+  };
+  // not sure why the ternary has to be done that way. Why doesn't it work with : "X"
+  return <div className="square" onClick={handleClick}>
+    {props.squareValue === "O" ? "O" : props.squareValue}
+  </div>;
+};
+
+export default Square;
